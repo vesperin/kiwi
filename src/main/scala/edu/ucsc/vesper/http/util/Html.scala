432fca8a2c01a8dac9bf0b6839bb9907e2113868
Huascar Sanchez <huascar.sanchez@gmail.com>
2014-07-12 19:28:46 -0700

    let's pretend the id is a static page; maybe this will allow google analytics to do its magic

--- src/main/scala/edu/ucsc/vesper/http/util/Html.scala
+++ src/main/scala/edu/ucsc/vesper/http/util/Html.scala
@@ -253,7 +253,7 @@ object Html {
               p(
                 a(href:="https://github.com/igrigorik/ga-beacon")(
                   img(
-                    src:= "https://ga-beacon.appspot.com/UA-52736669-3/kiwi/render?q=id:" + codeId + "?pixel",
+                    src:= "https://ga-beacon.appspot.com/UA-52736669-3/kiwi/render/" + codeId + "?pixel",
                     alt:= "Analytics", style:= "max-width:100%;"
                   )
                 )
