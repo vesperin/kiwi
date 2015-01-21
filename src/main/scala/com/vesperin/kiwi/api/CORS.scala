96e1dc233ec98b2171d672697ac2edb921696e14
Huascar Sanchez <huascar.sanchez@gmail.com>
2015-01-21 13:41:17 -0800

    adds new header option

--- src/main/scala/com/vesperin/kiwi/api/CORS.scala
+++ src/main/scala/com/vesperin/kiwi/api/CORS.scala
@@ -13,7 +13,7 @@ trait CORS {
 
   private val allowOriginHeader = `Access-Control-Allow-Origin`(AllOrigins)
   private val optionsCorsHeaders = List(
-    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"),
+    `Access-Control-Allow-Headers`("Origin, X-Requested-With, x-auth-token, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"),
     `Access-Control-Max-Age`(1728000)
   )
 
