8c0f958d132266b5ca029ac88aa56587484c0879
Huascar Sanchez <huascar.sanchez@gmail.com>
2015-01-21 13:42:13 -0800

    removes redundant brace

--- src/main/scala/com/vesperin/kiwi/api/CORS.scala
+++ src/main/scala/com/vesperin/kiwi/api/CORS.scala
@@ -19,13 +19,12 @@ trait CORS {
 
   def cors[T]: Directive0 = mapRequestContext { ctx => ctx.withRouteResponseHandling({
     //It is an option request for a resource that responds to some other method
-    case Rejected(x) if ctx.request.method.equals(HttpMethods.OPTIONS) && x.exists(_.isInstanceOf[MethodRejection]) => {
+    case Rejected(x) if ctx.request.method.equals(HttpMethods.OPTIONS) && x.exists(_.isInstanceOf[MethodRejection]) =>
       val allowedMethods: List[HttpMethod] = x.collect { case rejection: MethodRejection => rejection.supported }
       ctx.complete(HttpResponse().withHeaders(
         `Access-Control-Allow-Methods`(OPTIONS, allowedMethods: _*) :: allowOriginHeader ::
           optionsCorsHeaders
       ))
-    }
   }).withHttpResponseHeadersMapped { headers =>
     allowOriginHeader :: headers
   }}
